d23899a3bcde23e3fd63becc199178849c408303
Natanzin <FRUTOSDEGOIAS@10.30.34.163>
2020-02-12 21:56:18 -0200

    [ADD]DOA e BO da Atividade01

---
+++ Atividades/Atividade01/src/modelo/NegocioException.java
@@ -0,0 +1,14 @@
+//Author Natanzin
+package modelo;
+
+public class NegocioException extends Exception {
+
+    public NegocioException(String string) {
+        super(string);
+    }
+
+    public NegocioException(String string, Throwable thrwbl) {
+        super(string, thrwbl);
+    }
+
+}
